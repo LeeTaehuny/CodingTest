1649fae8c3f2b7822bc1f2df5538217d9858820f
LeeTaes <apth1023@naver.com>
2023-08-06 15:00:55 +0900

    practice

---
+++ practice/구현/1157 - 단어 공부.cpp
@@ -0,0 +1,62 @@
+﻿/** https://www.acmicpc.net/problem/1157 [구현, 문자열] */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string Word;
+int num[26];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> Word;
+
+	for (int i = 0; i < Word.size(); i++)
+	{
+		if (Word[i] >= 'A' && Word[i] <= 'Z')
+		{
+			num[Word[i] - 'A']++;
+		}
+		else if (Word[i] >= 'a' && Word[i] <= 'z')
+		{
+			num[Word[i] - 'a']++;
+		}
+	}
+
+	int max = -1;
+	int cnt = 0;
+	int maxIndex = 0;
+
+	for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+	{
+		if (num[i] > max)
+		{
+			max = num[i];
+			maxIndex = i;
+		}
+	}
+
+	for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+	{
+		if (num[i] == max)
+		{
+			cnt++;
+		}
+	}
+
+	char a = maxIndex + 'A';
+
+	if (cnt >= 2)
+	{
+		cout << "?" << endl;
+	}
+	else
+	{
+		cout << a << endl;
+	}
+}
