9aa85e4a3d28f488563ed4dfc6d480b771bf0dbd
LeeTaes <apth1023@naver.com>
2023-08-06 22:59:16 +0900

    practice solve

---
+++ practice/구현/11720 - 숫자의 합.cpp
@@ -0,0 +1,25 @@
+﻿/** https://www.acmicpc.net/problem/11720 [구현, 수학, 문자열] */
+
+// #include <conio.h> 헤더 인식 불가 (컴파일 오류 1번..)
+// "1d%"로 문자 1개만 입력받을 수 있음.
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int N;
+
+int main()
+{
+	cin >> N;
+
+	int sum = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		int num = 0;
+		scanf("%1d", &num);
+		sum = sum + num;
+	}
+	cout << sum;
+}
