b9d2240cdc1ace69be4d592a23d3161c6268a71a
LeeTaes <apth1023@naver.com>
2023-08-16 23:05:44 +0900

    practice solve

---
+++ practice/브루트포스 알고리즘/2798 - 블랙잭.cpp
@@ -0,0 +1,41 @@
+﻿/** https://www.acmicpc.net/problem/2798 [브루트포스 알고리즘] */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<int> v;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	int maxSum = -1;
+
+	for (int i = 0; i < N - 2; i++)
+	{
+		for (int j = i + 1; j < N - 1; j++)
+		{
+			for (int k = j + 1; k < N; k++)
+			{
+				if (maxSum < v[i] + v[j] + v[k] && v[i] + v[j] + v[k] <= M)
+				{
+					maxSum = v[i] + v[j] + v[k];
+				}
+			}
+		}
+	}
+
+	cout << maxSum;
+}
