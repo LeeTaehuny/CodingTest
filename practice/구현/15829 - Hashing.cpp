67ebe40f9794467a4e79d8fd24e06017b21043d6
LeeTaes <apth1023@naver.com>
2023-08-17 15:14:29 +0900

    practice solve

---
+++ practice/구현/15829 - Hashing.cpp
@@ -0,0 +1,39 @@
+﻿/** https://www.acmicpc.net/problem/15829 [구현, 문자열, 해싱] */
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+long long sum, result;
+int Arr[50];
+int r = 31;
+int m = 1234567891;
+
+int main()
+{
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		char temp;
+		cin >> temp;
+		Arr[i] = temp - 96;
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		sum = Arr[i];
+
+		for (int j = 0; j < i; j++)
+		{
+			sum = (sum * r) % m;
+			sum = sum % m;
+		}
+
+		result += sum;
+		result %= m;
+	}
+
+	cout << result;
+}
