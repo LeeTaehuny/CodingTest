a51bc333678eb3f2a3d6016d0d5626d8aebeff41
LeeTaes <apth1023@naver.com>
2023-08-18 15:47:54 +0900

    practice solve

---
+++ practice/구현/1259 - 팰린드롬수.cpp
@@ -0,0 +1,43 @@
+﻿/** https://www.acmicpc.net/problem/1259 [구현, 문자열] */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string str;
+vector<char> v;
+
+int main()
+{
+	while (true)
+	{
+		cin >> str;
+
+		if (str == "0")
+		{
+			exit(0);
+		}
+
+		for (int i = 0; i < (int)str.size(); i++)
+		{
+			v.push_back(str[i]);
+		}
+
+		vector<char> temp = v;
+		reverse(temp.begin(), temp.end());
+
+		if (v == temp)
+		{
+			cout << "yes" << '\n';
+		}
+		else
+		{
+			cout << "no" << '\n';
+		}
+
+		v.clear();
+		temp.clear();
+	}
+}
---
+++ practice/수학/1546 - 평균.cpp
@@ -0,0 +1,36 @@
+﻿/** https://www.acmicpc.net/problem/1546 [수학, 사칙연산] */
+
+#include <iostream>
+
+using namespace std;
+
+float Arr[1000];
+float N, sum;
+
+
+int main()
+{
+	cin >> N;
+
+	float max = -1;
+	int maxIdx = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> Arr[i];
+
+		if (max < Arr[i])
+		{
+			max = Arr[i];
+			maxIdx = i;
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		Arr[i] = Arr[i] / max * 100;
+		sum += Arr[i];
+	}
+
+	cout << (sum / N);
+}
