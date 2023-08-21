933f79d542443a5958d7da9bf2ba735978793892
LeeTaes <apth1023@naver.com>
2023-08-21 14:37:30 +0900

    solve

---
+++ chap02/[2-F] 사과 담기 게임/2_F.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N, M, J;
+int start = 1;
+int apple;
+int d;
+
+int main()
+{
+	cin >> N >> M;
+	cin >> J;
+
+	while (J--)
+	{
+		cin >> apple;
+
+		// 사이에 있는 경우
+		if (start <= apple && start + (M - 1) >= apple)
+		{
+			continue;
+		}
+		// 오른쪽에 있는 경우
+		if (start + (M - 1) < apple)
+		{
+			d = d + (apple - (start + (M - 1)));
+			start = apple - (M - 1);
+			continue;
+		}
+		// 왼쪽에 있는 경우
+		if (start > apple)
+		{
+			d = d + (start - apple);
+			start = apple;
+		}
+	}
+
+	cout << d;
+}
