2f837e0b05e52d4ec7677859bdf7243511fd579a
LeeTaes <apth1023@naver.com>
2023-08-22 22:14:48 +0900

    solve

---
+++ chap02/[2-G] 빈도 정렬/2_G.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+int N, C;
+
+int input[1004];
+map<int, int> m;
+
+int cnt, maxNum;
+
+int main()
+{
+	cin >> N >> C;
+
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+
+		cin >> temp;
+
+		if (m.find(temp) == m.end())
+		{
+			m.insert(make_pair(temp, 1));
+			input[i] = temp;
+		}
+		else
+		{
+			m[temp]++;
+		}
+	}
+
+	for (auto it = m.begin(); it != m.end(); it++)
+	{
+		if (maxNum < it->second)
+		{
+			maxNum = it->second;
+		}
+	}
+
+	while (maxNum > 0)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			if (maxNum == m[input[i]])
+			{
+				for (int j = 0; j < maxNum; j++)
+				{
+					cout << input[i] << " ";
+				}
+			}
+		}
+
+		maxNum--;
+	}
+}
