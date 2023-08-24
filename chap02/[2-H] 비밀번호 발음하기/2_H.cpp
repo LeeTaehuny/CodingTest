9dd61aeddf20c1559e7650d3b650b636269c7dbe
LeeTaes <apth1023@naver.com>
2023-08-24 18:35:49 +0900

    solve

---
+++ chap02/[2-H] 비밀번호 발음하기/2_H.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool isContain = false;
+bool isContinue = false;
+bool isSame = true;
+
+int JaCnt = 0;
+int MoCnt = 0;
+
+bool isMo = false;
+
+string Mo = "eoaiu";
+string str;
+
+int main()
+{
+	while (true)
+	{
+		cin >> str;
+
+		if (str == "end")
+		{
+			exit(0);
+		}
+
+		for (int i = 0; i < Mo.size(); i++)
+		{
+			if (find(str.begin(), str.end(), Mo[i]) != str.end())
+			{
+				isContain = true;
+				break;
+			}
+
+		}
+
+		for (int i = 0; i < str.size(); i++)
+		{
+			isMo = false;
+
+			for (int j = 0; j < Mo.size(); j++)
+			{
+				if (str[i] == Mo[j])
+				{
+					isMo = true;
+					break;
+				}
+			}
+
+			if (isMo)
+			{
+				MoCnt++;
+				JaCnt = 0;
+			}
+			else
+			{
+				JaCnt++;
+				MoCnt = 0;
+			}
+
+			if (MoCnt >= 3 || JaCnt >= 3)
+			{
+				isContinue = true;
+				break;
+			}
+		}
+
+		for (int i = 0; i < str.size() - 1; i++)
+		{
+			isSame = true;
+			if (str[i] == str[i + 1])
+			{
+				if (str[i] == 'e' || str[i] == 'o')
+				{
+					isSame = true;
+				}
+				else
+				{
+					isSame = false;
+					break;
+				}
+			}
+		}
+
+		if (isContain && !isContinue && isSame)
+		{
+			cout << "<" << str << "> is acceptable." << endl;
+		}
+		else
+		{
+			cout << "<" << str << "> is not acceptable." << endl;
+		}
+
+		JaCnt = 0;
+		MoCnt = 0;
+
+		isSame = true;
+		isContain = false;
+		isContinue = false;
+	}
+}
---
+++ chap02/[2-I] 수학숙제/2_I.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+string str;
+vector<string> v;
+
+bool cmp(string a, string b)
+{
+	if (a.length() < b.length())
+		return true;
+	if (a.length() == b.length())
+		return a.compare(b) < 0;
+
+	return false;
+}
+
+int main()
+{
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> str;
+		string temp = "";
+
+		for (int j = 0; j < (int)str.size(); j++)
+		{
+			if (str[j] >= '0' && str[j] <= '9')
+			{
+				if (temp != "")
+				{
+					temp = temp + str[j];
+				}
+				else
+				{
+					temp = str[j];
+				}
+			}
+			else
+			{
+				if (temp == "")
+				{
+					continue;
+				}
+
+				v.push_back(temp);
+				temp = "";
+			}
+		}
+
+		if (temp != "")
+		{
+			v.push_back(temp);
+		}
+	}
+
+	for (int i = 0; i < (int)v.size(); i++)
+	{
+		string temp = v[i];
+
+		while (true)
+		{
+			if (!temp.empty() && temp.front() == '0')
+			{
+				temp.erase(temp.begin());
+			}
+			else
+			{
+				if (!temp.empty())
+				{
+					v[i] = temp;
+					break;
+				}
+				else
+				{
+					v[i] = "0";
+					break;
+				}
+			}
+		}
+	}
+
+	sort(v.begin(), v.end(), cmp);
+
+	for (auto it = v.begin(); it != v.end(); it++)
+	{
+		cout << *it << '\n';
+	}
+}
