bb5306b0e13f8a9aaa2b9e7851b36f22c3f3211e
LeeTaes <apth1023@naver.com>
2023-08-28 19:37:48 +0900

    practice

---
+++ chap02/[2-J] 기상캐스터/2_J.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <string.h>
+#include <queue>
+using namespace std;
+
+int H, W;
+int adj[104][104];
+int visited[104][104];
+
+string temp;
+
+void BFS(int y, int x)
+{
+	visited[y][x] = 0;
+	queue<int>q;
+	q.push(x);
+
+	while (!q.empty())
+	{
+		x = q.front();
+		q.pop();
+
+		int nx = x + 1;
+
+		if (nx >= W || adj[y][nx] == 0)
+		{
+			continue;
+		}
+
+		visited[y][nx] = visited[y][x] + 1;
+		q.push(nx);
+	}
+}
+
+int main()
+{
+	memset(visited, -1, sizeof(visited));
+
+	cin >> H >> W;
+
+	for (int i = 0; i < H; i++)
+	{
+		cin >> temp;
+		for (int j = 0; j < W; j++)
+		{
+			if (temp[j] == '.')
+			{
+				adj[i][j] = -1;
+			}
+			else
+			{
+				adj[i][j] = 0;
+			}
+		}
+	}
+
+	for (int i = 0; i < H; i++)
+	{
+		for (int j = 0; j < W; j++)
+		{
+			if (adj[i][j] == 0)
+			{
+				BFS(i, j);
+			}
+		}
+	}
+
+	for (int i = 0; i < H; i++)
+	{
+		for (int j = 0; j < W; j++)
+		{
+			cout << visited[i][j] << " ";
+		}
+		cout << '\n';
+	}
+}
---
+++ chap02/[2-K] 교수가 된 현우/2_K.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int T, N;
+
+int cnt;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> T;
+
+	for (int i = 0; i < T; i++)
+	{
+		cin >> N;
+
+		for (int j = 5; j <= N; j *= 5)
+		{
+			cnt = cnt + (N / j);
+		}
+
+		cout << cnt << '\n';
+		cnt = 0;
+	}
+}
