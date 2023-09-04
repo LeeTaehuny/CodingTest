af65be036e9cce91d61ce91a06abb37a41b7362d
LeeTaes <apth1023@naver.com>
2023-09-04 22:26:43 +0900

    practice solve

---
+++ chap02/[2-P] 연구소/2_P.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <vector>
+#include <string.h>
+using namespace std;
+
+int N, M, cnt, wallCnt;
+int maxCnt = -1;
+int adj[10][10];
+bool visited[10][10];
+
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
+
+vector<pair<int, int>> area;
+
+void DFS(int y, int x)
+{
+	cnt++;
+	visited[y][x] = true;
+
+	for (int i = 0; i < 4; i++)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny < 0 || nx < 0 || ny >= N || nx >= M || visited[ny][nx])
+		{
+			continue;
+		}
+
+		if (adj[ny][nx] == 0)
+		{
+			DFS(ny, nx);
+		}
+	}
+}
+
+int main()
+{
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> adj[i][j];
+
+			if (adj[i][j] == 0)
+			{
+				area.push_back(make_pair(i, j));
+			}
+			else if (adj[i][j] == 1)
+			{
+				wallCnt++;
+			}
+		}
+	}
+
+	for (int i = 0; i < (int)area.size() - 2; i++)
+	{
+		for (int j = i + 1; j < (int)area.size() - 1; j++)
+		{
+			for (int k = j + 1; k < (int)area.size(); k++)
+			{
+				adj[area[i].first][area[i].second] = 1;
+				adj[area[j].first][area[j].second] = 1;
+				adj[area[k].first][area[k].second] = 1;
+
+				for (int y = 0; y < N; y++)
+				{
+					for (int x = 0; x < M; x++)
+					{
+						if (adj[y][x] == 2 && !visited[y][x])
+						{
+							DFS(y, x);
+						}
+					}
+				}
+
+				if ((N * M) - (wallCnt + 3) - cnt > maxCnt)
+				{
+					maxCnt = (N * M) - (wallCnt + 3) - cnt;
+				}
+
+				adj[area[i].first][area[i].second] = 0;
+				adj[area[j].first][area[j].second] = 0;
+				adj[area[k].first][area[k].second] = 0;
+
+				memset(visited, false, sizeof(visited));
+				cnt = 0;
+			}
+		}
+	}
+
+	cout << maxCnt;
+}
---
+++ chap02/[2-Q] 치즈/2_Q.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int Y, X, cnt;
+int adj[104][104];
+bool visited[104][104];
+bool moveAble[104][104];
+
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
+
+int k[100];
+
+
+void DFS(int y, int x)
+{
+	visited[y][x] = true;
+
+	for (int i = 0; i < 4; i++)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny < 0 || nx < 0 || ny >= Y || nx >= X || visited[ny][nx])
+		{
+			continue;
+		}
+
+		if (adj[ny][nx] == 1)
+		{
+			moveAble[ny][nx] = false;
+			continue;
+		}
+		else
+		{
+			DFS(ny, nx);
+		}
+
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> Y >> X;
+
+	for (int i = 0; i < Y; i++)
+	{
+		for (int j = 0; j < X; j++)
+		{
+			cin >> adj[i][j];
+
+			if (adj[i][j] == 1)
+			{
+				k[0]++;
+				moveAble[i][j] = true;
+			}
+		}
+	}
+
+	int idx = 1;
+
+	while (true)
+	{
+		DFS(0, 0);
+
+		for (int i = 0; i < Y; i++)
+		{
+			for (int j = 0; j < X; j++)
+			{
+				if (moveAble[i][j] == false)
+				{
+					adj[i][j] = 0;
+				}
+			}
+		}
+
+		for (int i = 0; i < Y; i++)
+		{
+			for (int j = 0; j < X; j++)
+			{
+				if (adj[i][j] == 1)
+				{
+					cnt++;
+				}
+			}
+		}
+
+		k[idx] = cnt;
+
+		if (cnt == 0)
+		{
+			break;
+		}
+
+		idx++;
+		cnt = 0;
+		memset(visited, false, sizeof(visited));
+	}
+
+	cout << idx << '\n';
+	cout << k[idx - 1] << '\n';
+}
