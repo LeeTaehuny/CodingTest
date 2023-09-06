a560e4e4cb0e4aece8c7dbafb3958505d237afb4
LeeTaes <apth1023@naver.com>
2023-09-06 22:18:05 +0900

    practice solve

---
+++ practice/수학/2609 - 최대공약수와 최소공배수.cpp
@@ -0,0 +1,48 @@
+﻿/** https://www.acmicpc.net/problem/2609 [수학, 정수론, 유클리드 호제법] */
+
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int Dmax;
+int Mmin;
+
+int main()
+{
+	cin >> N >> M;
+
+	// 최대 공약수
+	for (int i = 1; i < N + 1; i++)
+	{
+		if (N % i == 0 && M % i == 0)
+		{
+			Dmax = i;
+		}
+	}
+
+	int c_N = N;
+	int c_M = M;
+
+	// 최소 공배수
+	while (true)
+	{
+		if (N == M)
+		{
+			Mmin = N;
+			break;
+		}
+
+		if (N > M)
+		{
+			M += c_M;
+		}
+		else
+		{
+			N += c_N;
+		}
+	}
+
+	cout << Dmax << '\n';
+	cout << Mmin << '\n';
+}
