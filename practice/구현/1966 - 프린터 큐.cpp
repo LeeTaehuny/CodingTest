32f00457955ea582ab762def178bb7c294e63d20
LeeTaes <apth1023@naver.com>
2023-10-06 16:56:18 +0900

    practice solve

---
+++ practice/구현/1966 - 프린터 큐.cpp
@@ -0,0 +1,86 @@
+﻿/** https://www.acmicpc.net/problem/1966 [구현, 자료구조, 시뮬레이션, 큐] */
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+int n, m, idx;
+int cnt;
+
+vector<int> num;
+queue<pair<int, bool>> q;
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> m >> idx;
+
+		bool run = true;
+
+		for (int j = 0; j < m; j++)
+		{
+			int temp;
+			cin >> temp;
+
+			num.push_back(temp);
+
+			if (cnt != idx)
+			{
+				q.push(make_pair(temp, false));
+			}
+			else
+			{
+				q.push(make_pair(temp, true));
+			}
+
+			cnt++;
+		}
+
+		cnt = 0;
+
+		sort(num.begin(), num.end());
+
+		while (run)
+		{
+			int x = num[num.size() - 1 - cnt];
+
+			for (int j = 0; j < q.size(); j++)
+			{
+				if (x == q.front().first)
+				{
+					cnt++;
+
+					if (q.front().second == true)
+					{
+						cout << cnt << '\n';
+						run = false;
+						break;
+					}
+
+					q.pop();
+					break;
+				}
+				else
+				{
+					pair<int, bool> temp = q.front();
+					q.pop();
+					q.push(temp);
+				}
+			}
+		}
+
+		cnt = 0;
+
+		while (!q.empty())
+		{
+			q.pop();
+		}
+
+		num.clear();
+	}
+}
---
+++ practice/구현/2108 - 통계학.cpp
@@ -0,0 +1,109 @@
+﻿/** https://www.acmicpc.net/problem/2108 [구현, 수학, 정렬] */
+
+#include<iostream>
+#include<map>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n;
+map<int, int> m;
+
+float sum;
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		if (m.find(temp) != m.end())
+		{
+			m[temp]++;
+		}
+		else
+		{
+			m.insert(make_pair(temp, 1));
+		}
+	}
+
+	// 평균
+	for (auto it : m)
+	{
+		sum += it.first * it.second;
+	}
+
+	float temp = (sum / n) - (static_cast<int>(sum) / n);
+
+	if ((sum / n) >= 0 && abs(temp) >= 0.5f)
+	{
+		cout << (static_cast<int>(sum) / n) + 1 << '\n';
+	}
+	else if ((sum / n) < 0 && abs(temp) >= 0.5f)
+	{
+		cout << (static_cast<int>(sum) / n) - 1 << '\n';
+	}
+	else
+	{
+		cout << (static_cast<int>(sum) / n) << '\n';
+	}
+
+	// 중앙값
+	int cnt = 0;
+	int idx = n / 2;
+
+	for (auto it : m)
+	{
+		for (int i = 0; i < it.second; i++)
+		{
+			if (cnt == idx)
+			{
+				cout << it.first << '\n';
+			}
+			cnt++;
+		}
+	}
+
+	// 최빈값
+	int mn = -1;
+
+	for (auto it : m)
+	{
+		if (mn < it.second)
+		{
+			mn = it.second;
+		}
+	}
+
+	vector<int> v;
+
+	for (auto it : m)
+	{
+		if (it.second == mn)
+		{
+			v.push_back(it.first);
+		}
+	}
+
+	sort(v.begin(), v.end());
+
+	if (v.size() >= 2)
+		cout << v[1] << '\n';
+	else
+		cout << v[0] << '\n';
+
+	v.clear();
+
+	// 범위
+	for (auto it : m)
+	{
+		v.push_back(it.first);
+	}
+
+	sort(v.begin(), v.end());
+
+	cout << v.back() - v.front() << '\n';
+}
