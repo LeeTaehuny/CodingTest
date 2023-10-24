5fdf0efd9e19c05f04ad73659ee4b9c8633a6ac5
LeeTaes <apth1023@naver.com>
2023-10-24 14:19:20 +0900

    practice solve

---
+++ practice/탐색/1654 - 랜선자르기.cpp
@@ -0,0 +1,55 @@
+﻿/** https://www.acmicpc.net/problem/1654 [이분탐색, 매개변수 탐색] */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n, k;
+vector<long long> v;
+
+int main()
+{
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	sort(v.begin(), v.end());
+
+	long long s = 1;
+	long long e = v[n - 1];
+	long long mid;
+
+	while (true)
+	{
+		int cnt = 0;
+
+		mid = (s + e) / 2;
+
+		for (int i = 0; i < n; i++)
+		{
+			cnt += v[i] / mid;
+		}
+
+		if (s > e)
+		{
+			break;
+		}
+
+		if (cnt >= k)
+		{
+			s = mid + 1;
+		}
+		else if (cnt < k)
+		{
+			e = mid - 1;
+		}
+	}
+
+	cout << mid;
+}
