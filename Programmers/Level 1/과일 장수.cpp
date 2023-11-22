edbe41b62bcc41429098f48c0d5cca93ca56b5fd
LeeTaes <apth1023@naver.com>
2023-11-22 18:19:54 +0900

    programmers solve

---
+++ Programmers/Level 1/과일 장수.cpp
@@ -0,0 +1,27 @@
+﻿/** 
+[ 링크 ] https://school.programmers.co.kr/learn/courses/30/lessons/135808
+
+[ 풀이 방법 ]
+
+1. 낮은 순으로 과일을 정렬합니다.
+2. 뒤에서부터 m개씩 건너뛰며 m번째 과일의 값 * m을 answer에 저장합니다.
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+
+    sort(score.begin(), score.end());
+
+    for (auto it = score.end() - m; it >= score.begin(); it -= m)
+    {
+        answer += *it * m;
+    }
+
+    return answer;
+}
