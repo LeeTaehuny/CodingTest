d6251e924201c2170a9af47306c3206931c04ec8
LeeTaes <apth1023@naver.com>
2024-01-28 17:35:21 +0900

    practice solve

---
+++ chap05/5_A 순회강연.cpp
@@ -0,0 +1,114 @@
+﻿https://www.acmicpc.net/problem/2109
+
+![boj-og.png](https://prod-files-secure.s3.us-west-2.amazonaws.com/5bcdc658-500f-4667-9ab4-68d815b2e44d/f5b9d047-36db-4085-bf61-f7c2e58cdb2b/boj-og.png)
+
+## 문제
+
+한 저명한 학자에게 n(0 ≤ n ≤ 10,000)개의 대학에서 강연 요청을 해 왔다. 각 대학에서는 d(1 ≤ d ≤ 10,000)일 안에 와서 강연을 해 주면 p(1 ≤ p ≤ 10,000)만큼의 강연료를 지불하겠다고 알려왔다. 각 대학에서 제시하는 d와 p값은 서로 다를 수도 있다. 이 학자는 이를 바탕으로, 가장 많은 돈을 벌 수 있도록 순회강연을 하려 한다. 강연의 특성상, 이 학자는 하루에 최대 한 곳에서만 강연을 할 수 있다.
+
+예를 들어 네 대학에서 제시한 p값이 각각 50, 10, 20, 30이고, d값이 차례로 2, 1, 2, 1 이라고 하자. 이럴 때에는 첫째 날에 4번 대학에서 강연을 하고, 둘째 날에 1번 대학에서 강연을 하면 80만큼의 돈을 벌 수 있다.
+
+## 입력
+
+첫째 줄에 정수 n이 주어진다. 다음 n개의 줄에는 각 대학에서 제시한 p값과 d값이 주어진다.
+
+## 예제 입력 1
+
+```
+7
+20 1
+2 1
+10 3
+100 2
+8 2
+5 20
+50 10
+
+```
+
+## 예제 출력 1
+
+```
+185
+```
+
+## 코드
+
+```cpp
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+// N : 대학의 수
+// D : 강연 날짜
+// P : 강연료
+int n;
+int d, p;
+
+// 대학별 일자, 강연료 리스트
+vector<pair<int, int>> v;
+
+// 결과 반환용
+int res;
+
+bool comp(pair<int, int> n1, pair<int, int> n2)
+{
+	return n1.first > n2.first;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+
+	int mxD;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> p >> d;
+
+		// 날짜, 금액 순으로 저장
+		v.push_back({ d, p });
+
+		mxD = max(mxD, d);
+	}
+
+	// 높은 날짜 순으로 정렬
+	sort(v.begin(), v.end(), comp);
+
+	int idx = 0;
+
+	priority_queue<int, vector<int>, less<int>> pq;
+
+	// 마지막 날짜부터 해당 날짜에 맞는 작업들 push
+	for (int i = mxD; i > 0; i--)
+	{
+		while (idx < v.size())
+		{
+			if (v[idx].first == i)
+			{
+				pq.push(v[idx].second);
+				idx++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		// 해당 날짜에서 수행할 수 있는 가장 큰 일 수행
+		if (pq.size())
+		{
+			res += pq.top();
+			pq.pop();
+		}
+	}
+
+	cout << res;
+}
+```
